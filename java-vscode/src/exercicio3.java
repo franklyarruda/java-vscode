d2f851615fa83c715ad91c8c787eb04d196a6c67
Frankly <frankly.arruda@gmail.com>
2022-10-17 00:01:50 -0300

    exercicio 3

---
+++ java-vscode/src/exercicio3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+    public static void main(String[] args) {
+        
+//3. Escreva um programa para ler um nome digitado e mostrar a mensagem "Olá <nome-digitado>".
+
+Scanner entrada = new Scanner(System.in);
+System.out.println("qual seu nome ");
+String nome = entrada.nextLine();
+
+System.out.println("Olá " + nome);
+
+    }
+}
