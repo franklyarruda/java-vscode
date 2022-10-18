6ee0ba09f52581ea8039ee2b7f824afb869fec6b
Frankly <frankly.arruda@gmail.com>
2022-10-18 19:49:54 -0300

    exercicioRepetição

---
+++ java-vscode/src/exemplorepeticao.java
@@ -0,0 +1,20 @@
+public class exemplorepeticao {
+
+    public static void main(String[] args) {
+        
+        int cont = 1;
+        while(cont <= 10){
+        System.out.println(cont);
+        cont ++;
+        }
+
+
+        do{
+            cont --;
+            System.out.println(cont);
+        }while(cont >= 1);
+            
+        
+    }
+    
+}
