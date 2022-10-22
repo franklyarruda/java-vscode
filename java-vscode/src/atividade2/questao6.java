1b1b9a3ac9d99e79b446f1052ed27a433dbf6b0d
Frankly <frankly.arruda@gmail.com>
2022-10-22 19:16:12 -0300

    questao 6

--- java-vscode/src/atividade2/questao6.java
+++ java-vscode/src/atividade2/questao6.java
@@ -13,31 +13,33 @@ public class questao6 {
         int menor = 0;
         int soma = 0;
         int media = 0;
-
-        int n1 = 0;
+        
 
         for (int i = 0; i < 5; i++) {
+
             System.out.println("digitar um numero: ");
             Scanner entrada = new Scanner(System.in);
-            n1 = entrada.nextInt();
+            int n1 = entrada.nextInt();
 
             if (n1 > maior) {
                 maior = n1;
-            }else{
-
                 menor = n1;
+                System.out.println("maior " + maior);
+                
             }
             if (n1 < menor) {
                 menor = n1;
+                System.out.println("menor " + menor);
             }
-            
             soma = soma + n1;
             media = (soma / 5);
+
         }
-            
+        System.out.println();
         System.out.println("maior " + maior);
         System.out.println("menor " + menor);
         System.out.println("soma dos numeros " + soma);
-        System.out.println("media" + media);
+        System.out.println("media: " + media);
+
     }
 }
