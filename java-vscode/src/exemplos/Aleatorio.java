56e22044477ab08d24469a886c72b18a61fe0e6b
Frankly <frankly.arruda@gmail.com>
2022-11-04 20:08:12 -0300

    projeto spring

---
+++ java-vscode/src/exemplos/Aleatorio.java
@@ -0,0 +1,16 @@
+package exemplos;
+
+public class Aleatorio {
+    
+public static void main(String[] args) {
+    
+    for(int i = 1; i < 20; i++){
+        int maior = 3;
+        System.out.println(Math.round(Math.random()* maior ));
+        if ()
+    
+}
+
+}
+
+}
